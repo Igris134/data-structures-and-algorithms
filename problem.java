fcaf88fed054ac6596edba556096f48953390507
Jusper Namoc <juspernamoc35@gmail.com>
2025-11-25 11:42:40 +0800

    Add files via upload

---
+++ problem.java
@@ -0,0 +1,302 @@
+import java.util.Scanner;
+public class problem {
+    public static void main(String[] args)
+	{
+		menu();
+	}
+	public static void menu()
+	{
+		Scanner in = new Scanner(System.in);
+				int choice = 1;
+				while(choice!=0)
+				{
+				System.out.print("choose 1-56 to run a problem\n");
+				System.out.println("choose 0 to exit\n");
+				choice = in.nextInt();
+				switch(choice)
+				{
+					case 1:
+						MyFunctions.problemNo1();
+						break;
+					case 2:
+						MyFunctions.problemNo2();
+						break;
+					case 3:
+						MyFunctions mf = new MyFunctions();
+						mf.problemNo3();
+						break;
+					case 4:
+						MyFunctions distance = new MyFunctions();
+                        distance.problemNo4();
+						break;
+					case 5:
+						MyFunctions amount = new MyFunctions();
+                        amount.problemNo5();
+						break;
+                    case 6:
+                        MyFunctions p6 = new MyFunctions();
+                        p6.problemNo6();
+                        break;
+
+                    case 7:
+                        MyFunctions p7 = new MyFunctions();
+                        p7.problemNo7();
+                        break;
+
+                    case 8:
+                        MyFunctions p8 = new MyFunctions();
+                        p8.problemNo8();
+                        break;
+
+                    case 9:
+                        MyFunctions p9 = new MyFunctions();
+                        p9.problemNo9();
+                        break;
+
+                    case 10:
+                        MyFunctions p10 = new MyFunctions();
+                        p10.problemNo10();
+                        break;
+
+                    case 11:
+                        MyFunctions p11 = new MyFunctions();
+                        p11.problemNo11();
+                        break;
+
+                    case 12:
+                        MyFunctions p12 = new MyFunctions();
+                        p12.problemNo12();
+                        break;
+
+                    case 13:
+                        MyFunctions p13 = new MyFunctions();
+                        p13.problemNo13();
+                        break;
+
+                    case 14:
+                        MyFunctions p14 = new MyFunctions();
+                        p14.problemNo14();
+                        break;
+
+                    case 15:
+                        MyFunctions p15 = new MyFunctions();
+                        p15.problemNo15();
+                        break;
+
+                    case 16:
+                        MyFunctions p16 = new MyFunctions();
+                        p16.problemNo16();
+                        break;
+
+                    case 17:
+                        MyFunctions p17 = new MyFunctions();
+                        p17.problemNo17();
+                        break;
+
+                    case 18:
+                        MyFunctions p18 = new MyFunctions();
+                        p18.problemNo18();
+                        break;
+
+                    case 19:
+                        MyFunctions p19 = new MyFunctions();
+                        p19.problemNo19();
+                        break;
+
+                    case 20:
+                        MyFunctions p20 = new MyFunctions();
+                        p20.problemNo20();
+                        break;
+
+                    case 21:
+                        MyFunctions p21 = new MyFunctions();
+                        p21.problemNo21();
+                        break;
+
+                    case 22:
+                        MyFunctions p22 = new MyFunctions();
+                        p22.problemNo22();
+
+                        break;
+
+                    case 23:
+                        MyFunctions p23 = new MyFunctions();
+                        p23.problemNo23();
+                        break;
+
+                    case 24:
+                        MyFunctions p24 = new MyFunctions();
+                        p24.problemNo24();
+                        break;
+
+                    case 25:
+                        MyFunctions p25 = new MyFunctions();
+                        p25.problemNo25();
+                        break;
+
+                    case 26:
+                        MyFunctions p26 = new MyFunctions();
+                        p26.problemNo26();
+                        break;
+
+                    case 27:
+                        MyFunctions p27 = new MyFunctions();
+                        p27.problemNo27();
+                        break;
+
+                    case 28:
+                        MyFunctions p28 = new MyFunctions();
+                        p28.problemNo28();
+                        break;
+
+                    case 29:
+                        MyFunctions p29 = new MyFunctions();
+                        p29.problemNo29();
+                        break;
+
+                    case 30:
+                        MyFunctions p30 = new MyFunctions();
+                        p30.problemNo30();
+                        break;
+
+                    case 31:
+                        MyFunctions p31 = new MyFunctions();
+                        p31.problemNo31();
+                        break;
+
+                    case 32:
+                        MyFunctions p32 = new MyFunctions();
+                        p32.problemNo32();
+                        break;
+
+                    case 33:
+                        MyFunctions p33 = new MyFunctions();
+                        p33.problemNo33();
+                        break;
+
+                    case 34:
+                        MyFunctions p34 = new MyFunctions();
+                        p34.problemNo34();
+                        break;
+
+                    case 35:
+                        MyFunctions p35 = new MyFunctions();
+                        p35.problemNo35();
+                        break;
+
+                    case 36:
+                        MyFunctions p36 = new MyFunctions();
+                        p36.problemNo36();
+                        break;
+
+                    case 37:
+                        MyFunctions p37 = new MyFunctions();
+                        p37.problemNo37();
+                        break;
+
+                    case 38:
+                        MyFunctions p38 = new MyFunctions();
+                        p38.problemNo38();
+                        break;
+
+                    case 39:
+                        MyFunctions p39 = new MyFunctions();
+                        p39.problemNo39();
+                        break;
+
+                    case 40:
+                        MyFunctions p40 = new MyFunctions();
+                        p40.problemNo40();
+                        break;
+
+                    case 41:
+                        MyFunctions p41 = new MyFunctions();
+                        p41.problemNo41();
+                        break;
+
+                    case 42:
+                        MyFunctions p42 = new MyFunctions();
+                        p42.problemNo42();
+                        break;
+
+                    case 43:
+                        MyFunctions p43 = new MyFunctions();
+                        p43.problemNo43();
+                        break;
+
+                    case 44:
+                        MyFunctions p44 = new MyFunctions();
+                        p44.problemNo44();
+                        break;
+
+                    case 45:
+                        MyFunctions p45 = new MyFunctions();
+                        p45.problemNo45();
+                        break;
+
+                    case 46:
+                        MyFunctions p46 = new MyFunctions();
+                        p46.problemNo46();
+                        break;
+
+                    case 47:
+                        MyFunctions p47 = new MyFunctions();
+                        p47.problemNo47();
+                        break;
+
+                    case 48:
+                        MyFunctions p48 = new MyFunctions();
+                        p48.problemNo48();
+                        break;
+
+                    case 49:
+                        MyFunctions p49 = new MyFunctions();
+                        p49.problemNo49();
+                        break;
+
+                    case 50:
+                        MyFunctions p50 = new MyFunctions();
+                        p50.problemNo50();
+                        break;
+
+                    case 51:
+                        MyFunctions p51 = new MyFunctions();
+                        p51.problemNo51();
+                        break;
+
+                    case 52:
+                        MyFunctions p52 = new MyFunctions();
+                        p52.problemNo52();
+                        break;
+
+                    case 53:
+                        MyFunctions p53 = new MyFunctions();
+                        p53.problemNo53();
+                        break;
+
+                    case 54:
+                        MyFunctions p54 = new MyFunctions();
+                        p54.problemNo54();
+                        break;
+
+                    case 55:
+                        MyFunctions p55 = new MyFunctions();
+                        p55.problemNo55();
+                        break;
+
+                    case 56:
+                        MyFunctions p56 = new MyFunctions();
+                        p56.problemNo56();
+                        break;
+                        
+					case 0:
+						System.out.println("Thank you for using my program");
+						System.exit(0);
+						break;
+					default:
+						System.out.println("not yet implemented");
+						break;
+				}
+	  }
+	}
+}
